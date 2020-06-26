e0e411bcc066ae91dad0107f559647dfe87d1ce0
nikza <nikitos86902002@gmail.com>
2020-06-27 00:22:42 +0300

    Base structure

--- app/src/main/java/com/nikza/ituts/MainActivity.java
+++ app/src/main/java/com/nikza/ituts/MainActivity.java
@@ -2,14 +2,32 @@ package com.nikza.ituts;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
     }
+    private void initToolbar(){
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
 }
